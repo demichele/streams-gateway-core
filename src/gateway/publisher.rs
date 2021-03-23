c43fb19b8993b397fa36504e825ee9293b650a67
Daniel De Michele <carpdemichele@gmail.com>
2021-03-23 16:29:51 -0300

    Update publisher.rs

--- src/gateway/publisher.rs
+++ src/gateway/publisher.rs
@@ -61,7 +61,7 @@ impl Channel {
     ///
     /// Open a channel
     ///
-    #[tokio::open]
+    #[tokio::main]
     pub async fn open(&mut self) -> Result<(String, String)> {
         let announcement_message = self.author.send_announce()?;
 
